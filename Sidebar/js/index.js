569598f4f6d5a1f17b48206709355a155588552c
kerimov528 <kerimov.anar528@gmail.com>
2022-06-21 14:03:56 -0700

    Sidebar Completed

---
+++ Sidebar/js/index.js
@@ -0,0 +1,14 @@
+const sidebar = document.querySelector('.sidebar');
+const menuBtn = document.querySelector('#menu-span');
+const closeBtn = document.querySelector('#close-span');
+
+closeBtn.addEventListener('click', () => {
+ if (!sidebar.classList.contains('hide__sidebar')) {
+  sidebar.classList.add('hide__sidebar');
+ }
+});
+menuBtn.addEventListener('click', () => {
+ if (sidebar.classList.contains('hide__sidebar')) {
+  sidebar.classList.remove('hide__sidebar');
+ }
+});
